ed14ddd5d628b19513ee13ecc10a231b63bad778
Cedric Hauber <chauber@onewealthplace.com>
2026-03-22 19:35:11 +0100

    Mise à jour de la configuration PlatformIO pour exclure les fichiers de sauvegarde ros_lib_backup et ajustement de la gestion des nœuds ROS pour STM32.

--- stm32/ros_usbnode/src/ros/ros_lib/ros.h
+++ stm32/ros_usbnode/src/ros/ros_lib/ros.h
@@ -1,4 +1,4 @@
-/*
+/* 
  * Software License Agreement (BSD License)
  *
  * Copyright (c) 2011, Willow Garage, Inc.
@@ -36,32 +36,11 @@
 #define _ROS_H_
 
 #include "ros/node_handle.h"
-
-#if defined(ESP8266) or defined(ESP32) or defined(ROSSERIAL_ARDUINO_TCP)
-  #include "ArduinoTcpHardware.h"
-#else
-  #include "ArduinoHardware.h"
-#endif
+#include "STM32Hardware.h"
 
 namespace ros
 {
-#if defined(__AVR_ATmega8__) or defined(__AVR_ATmega168__)
-  /* downsize our buffers */
-  typedef NodeHandle_<ArduinoHardware, 6, 6, 150, 150> NodeHandle;
-
-#elif defined(__AVR_ATmega328P__)
-
-  typedef NodeHandle_<ArduinoHardware, 25, 25, 280, 280> NodeHandle;
-
-#elif defined(SPARK)
-
-  typedef NodeHandle_<ArduinoHardware, 10, 10, 2048, 2048> NodeHandle;
-
-#else
-
-  typedef NodeHandle_<ArduinoHardware> NodeHandle; // default 25, 25, 512, 512
-
-#endif
+  typedef ros::NodeHandle_<STM32Hardware> NodeHandle;
 }
 
 #endif
